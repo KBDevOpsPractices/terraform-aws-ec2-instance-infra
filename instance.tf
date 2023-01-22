3046538053a7f8ceb50ed2a87f4dce11fb22f4d1
Kishor Bankar <kishorbankar@Kishors-MacBook-Pro.local>
2023-01-22 20:32:20 +0530

    security group

--- instance.tf
+++ instance.tf
@@ -7,11 +7,29 @@ resource "aws_key_pair" "ec2-ssh-key" {
   }
 }
 
-resource "aws_instance" "web-ec2-instance" {
-  ami           = var.aws_ami_id
-  instance_type = var.instance_type
+resource "aws_security_group" "security_group_allow_tls" {
+  name        = "security_group_allow_tls"
+  description = "Allow TLS inbound traffic"
 
-  key_name = aws_key_pair.ec2-ssh-key.key_name
+  dynamic "ingress" {
+    for_each = [22, 80, 443]
+    iterator = port
+    content {
+      description = "TLS from VPC"
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+}
+
+resource "aws_instance" "web-ec2-instance" {
+  ami             = var.aws_ami_id
+  instance_type   = var.instance_type
+  key_name        = aws_key_pair.ec2-ssh-key.key_name
+  security_groups = [aws_security_group.security_group_allow_tls.id]
 
   tags = {
     Name = "aws-devops-tf"
