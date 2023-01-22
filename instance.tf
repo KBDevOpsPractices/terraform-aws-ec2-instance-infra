0a1640e8f28a3fc9a72f5f108774482be9a55d2b
Kishor Bankar <kishorbankar@Kishors-MacBook-Pro.local>
2023-01-22 20:35:14 +0530

    security group

--- instance.tf
+++ instance.tf
@@ -26,10 +26,10 @@ resource "aws_security_group" "security_group_allow_tls" {
 }
 
 resource "aws_instance" "web-ec2-instance" {
-  ami             = var.aws_ami_id
-  instance_type   = var.instance_type
-  key_name        = aws_key_pair.ec2-ssh-key.key_name
-  security_groups = [aws_security_group.security_group_allow_tls.id]
+  ami                    = var.aws_ami_id
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.ec2-ssh-key.key_name
+  vpc_security_group_ids = [aws_security_group.security_group_allow_tls.id]
 
   tags = {
     Name = "aws-devops-tf"
