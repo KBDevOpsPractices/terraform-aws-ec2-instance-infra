be7007c1b4bb82e3e681825db0879ccb421c22ad
Kishor Bankar <kishorbankar@Kishors-MacBook-Pro.local>
2023-01-22 20:50:43 +0530

    security group

--- instance.tf
+++ instance.tf
@@ -12,14 +12,14 @@ resource "aws_security_group" "security_group_allow_tls" {
   description = "Allow TLS inbound traffic"
 
   dynamic "ingress" {
-    for_each = [22, 80, 443]
+    for_each = var.ports
     iterator = port
     content {
-      description      = "TLS from VPC"
-      from_port        = port.value
-      to_port          = port.value
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
+      description = "TLS from VPC"
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
 
     }
   }
--- vars/development.tfvars
+++ vars/development.tfvars
@@ -1,2 +1,3 @@
 aws_ami_id="ami-00874d747dde814fa"
 instance_type="t2.micro"
+ports = [22, 80, 443]
