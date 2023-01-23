f09294b3989be03984bfea057598143eb39e086d
Kishor Bankar <kishorbankar@Kishors-MacBook-Pro.local>
2023-01-23 20:22:25 +0530

    update

--- aws-keypair.tf
+++ aws-keypair.tf
@@ -1,6 +1,6 @@
 # ssh-key
 resource "aws_key_pair" "ec2-ssh-key" {
-  key_name   = "devops-ec2-ssh-key-tf"
+  key_name   = "devops-aws-ssh-key-tf"
   public_key = file("${path.module}/id_rsa.pub")
   tags       = local.common_tags
 }
